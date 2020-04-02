a1537a00d9719695b3a7bb14acf28783c0ba32af
Kees van Drongelen <kees@visualspace.nl>
2020-04-02 15:59:04 +0200

    Introducing cards for search fragments. This is better than list views. In here we ca easily put video, audio and text fragments.

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -7,7 +7,10 @@
     <v-container class="mx-auto pa-0 pt-4">
       <v-container class="mt-n3 mb-n6">
         <v-row justify="end">
-          <v-col cols="12" class="pb-0 mb-0">
+          <v-col cols="12" class="result-header mb-n6 pb-0 mb-0">
+            <p class="text-right">We found 4320 results in 288 pages!</p>
+          </v-col>
+          <v-col cols="12" class="pb-0 mb-n2">
             <v-select
               v-model="selectedSearchableCategories"
               :items="searchableCategories"
@@ -37,152 +40,120 @@
         </v-row>
       </v-container>
 
-      <v-subheader class="mt-n2">August</v-subheader>
-      <v-list three-line class="mt-n2 mb-2">
-        <template v-for="(item, index) in entrees">
-          <v-divider
-            v-if="item.divider"
-            :key="index"
-            :inset="item.inset"
-          ></v-divider>
+      <v-container>
+        <v-row dense>
+          <v-col cols="12">
+            <v-subheader class="mt-n2 mb-n4">Texts (230) - view all</v-subheader>
+          </v-col>
+        </v-row>
+        <v-row dense>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title class="subtitle-1">Unlimited music now</v-card-title>
 
-          <v-list-item
-            v-else
-            :key="item.title"
-            @click="doSomething"
-          >
+              <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
 
-            <v-list-item-content>
-              <v-list-item-title v-html="item.title"></v-list-item-title>
-              <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-list>
+              <!-- <v-card-actions>
+                <v-btn text>Listen Now</v-btn>
+              </v-card-actions> -->
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title class="subtitle-1">Unlimited music now</v-card-title>
 
-      <v-subheader>Grouped objects</v-subheader>
-      <v-item-group multiple class="mt-n6">
-        <v-container>
-          <v-row>
-            <v-col
-              v-for="n in 6"
-              :key="n"
-              cols="6"
-              sm="4"
-              md="3"
-              lg="2"
-            >
-              <v-item v-slot:default="{ active, toggle }">
-                <v-card
-                  :color="active ? 'primary' : ''"
-                  class="d-flex align-center"
-                  dark
-                  height="200"
-                  @click="toggle"
+              <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+              <!-- <v-card-actions>
+                <v-btn text>Listen Now</v-btn>
+              </v-card-actions> -->
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title class="subtitle-1">Unlimited music now</v-card-title>
+
+              <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+              <!-- <v-card-actions>
+                <v-btn text>Listen Now</v-btn>
+              </v-card-actions> -->
+            </v-card>
+          </v-col>
+          <v-col cols="12">
+            <v-subheader class="mt-n2 mb-n4"></v-subheader>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-container>
+        <v-row dense>
+          <v-col cols="12">
+            <v-subheader class="mt-n2 mb-n4">Audio (40)</v-subheader>
+          </v-col>
+        </v-row>
+        <v-row dense>
+          <v-col cols="12">
+            <v-card>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title
+                    class="subtitle-1"
+                  >Supermodel</v-card-title>
+                  <v-card-subtitle>Foster the People</v-card-subtitle>
+                </div>
+                <v-avatar
+                  class="ma-2"
+                  size="125"
+                  tile
                 >
-                  <v-scroll-y-transition>
-                    <div
-                      v-if="active"
-                      class="display-3 flex-grow-1 text-center"
-                    >
-                      Active
-                    </div>
-                  </v-scroll-y-transition>
-                </v-card>
-              </v-item>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-item-group>
-
-
-      <v-subheader>May</v-subheader>
-      <v-container fluid class="mt-n6">
-        <v-row>
-          <v-col
-            v-for="i in 6"
-            :key="i"
-            cols="6"
-            sm="4"
-            md="3"
-            lg="2"
-          >
-            <img
-              :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`"
-              alt="lorem"
-              class="image"
-              height="100%"
-              width="100%"
-            >
+                  <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"></v-img>
+                </v-avatar>
+              </div>
+            </v-card>
           </v-col>
-        </v-row>
-      </v-container>
+          <v-col cols="12">
+            <v-card>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title
+                    class="subtitle-1"
+                  >Halcyon Days</v-card-title>
 
-      <v-subheader>June</v-subheader>
-      <v-container fluid class="mt-n6">
-        <v-row>
-          <v-col
-            v-for="i in 6"
-            :key="i"
-            cols="6"
-            sm="4"
-            md="3"
-            lg="2"
-          >
-            <img
-              :src="`https://randomuser.me/api/portraits/women/${i + 5}.jpg`"
-              alt="lorem"
-              class="image"
-              height="100%"
-              width="100%"
-            >
+                  <v-card-subtitle>Ellie Goulding</v-card-subtitle>
+                </div>
+
+                <v-avatar
+                  class="ma-2"
+                  size="125"
+                  tile
+                >
+                  <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
+                </v-avatar>
+              </div>
+            </v-card>
           </v-col>
-        </v-row>
-      </v-container>
-
-      <v-subheader>August</v-subheader>
-      <v-container fluid class="mt-n6">
-        <v-row>
-          <v-col
-            v-for="n in 9"
-            :key="n"
-            class="d-flex child-flex"
-            cols="6"
-            sm="4"
-            md="3"
-            lg="2"
-          >
-            <v-card flat tile class="d-flex">
-              <v-img
-                :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                aspect-ratio="1"
-                class="grey lighten-2"
-              >
-                <template v-slot:placeholder>
-                  <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
-                  >
-                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                  </v-row>
-                </template>
-              </v-img>
+          <v-col cols="12">
+            <v-card>
+              <div class="d-flex flex-no-wrap justify-space-between">
+                <div>
+                  <v-card-title
+                    class="subtitle-1"
+                  >Supermodel</v-card-title>
+                  <v-card-subtitle>Foster the People</v-card-subtitle>
+                </div>
+                <v-avatar
+                  class="ma-2"
+                  size="125"
+                  tile
+                >
+                  <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"></v-img>
+                </v-avatar>
+              </div>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
-
-      <template >
-        <div class="mt-4">
-          <v-pagination
-            v-model="page"
-            :length="15"
-            :total-visible="7"
-          ></v-pagination>
-        </div>
-      </template>
 
       <template>
         <v-footer min-height="36px" color="white">
@@ -192,6 +163,16 @@
       </template>
 
     </v-container>
+
+    <template >
+      <div class="mt-4 mb-8">
+        <v-pagination
+          v-model="page"
+          :length="15"
+          :total-visible="7"
+        ></v-pagination>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -211,36 +192,36 @@ export default {
   data: () => ({
     page: 1,
     query: null,
-    entrees: [
-      { divider: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-        title: 'Brunch this weekend?',
-        subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-      },
-      { divider: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-        subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
-      },
-      { divider: true },
-      {
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-        title: 'Oui oui',
-        subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
-      },
-      { divider: true },
-    ],
+    // entrees: [
+    //   { divider: true },
+    //   {
+    //     avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+    //     title: 'Brunch this weekend?',
+    //     subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+    //   },
+    //   { divider: true },
+    //   {
+    //     avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+    //     title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+    //     subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+    //   },
+    //   { divider: true },
+    //   {
+    //     avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+    //     title: 'Oui oui',
+    //     subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+    //   },
+    //   { divider: true },
+    // ],
 
     selectedSearchableCategories: [],
 
     searchableCategories: [
-      'Texts',
-      'Images',
-      'Music',
-      'Videos',
-      'Directories',
+      'Texts (230)',
+      'Images (741)',
+      'Music (40)',
+      'Videos (25)',
+      'Directories (400)',
     ],
   }),
 
@@ -286,5 +267,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+  .results {
+    .result-header {
+      p {
+        font-size: 80%;
+        opacity: 0.6;
+      }
+    }
+    .v-card {
+      .headline {
+        font-size: 75%;
+      }
+    }
+  }
 </style>
